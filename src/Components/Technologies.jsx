10e3f56739b52c2da599cf122390dabf034a80bd
Kasra-ebr <kasraebr1378@gmail.com>
2025-07-10 09:19:22 -0400

    Commit

--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -1,79 +1,92 @@
 import React from "react";
 export const technologies = [
-
   {
+    id: 1,
     name: "React",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
   },
   {
+    id: 2,
     name: "Tailwind CSS",
     src: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
   },
   {
+    id: 3,
     name: "JavaScript",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
   },
   {
+    id: 4,
     name: "TypeScript",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
   },
   {
+    id: 5,
     name: "HTML5",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
   },
-
- 
   {
+    id: 6,
     name: "Git",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
   },
   {
+    id: 7,
     name: "GitHub",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
   },
   {
+    id: 8,
     name: "npm",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg",
   },
   {
+    id: 9,
     name: "Formik",
     src: "https://formik.org/images/favicon.png",
   },
   {
+    id: 10,
     name: "Redux",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg",
   },
   {
+    id: 11,
     name: "MongoDB",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
   },
   {
+    id: 12,
     name: "Postman",
     src: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
   },
   {
+    id: 13,
     name: "Jest",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg",
   },
   {
+    id: 14,
     name: "Firebase",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg",
   },
   {
+    id: 15,
     name: "GraphQL",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/graphql/graphql-plain.svg",
   },
-
-
   {
+    id: 16,
     name: "Flutter",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg",
   },
   {
+    id: 17,
     name: "Figma",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg",
   },
   {
+    id: 18,
     name: "Sass",
     src: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg",
   },
@@ -93,8 +106,8 @@ function Technologies() {
         </div>
 
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
-          {technologies?.map(({ name, src, index }) => (
-            <TechCard key={name} name={name} src={src} />
+          {technologies?.map(({ name, src, id }) => (
+            <TechCard key={id} name={name} src={src} />
           ))}
         </div>
       </div>
