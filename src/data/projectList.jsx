3b31cfaecbe66a41d398bc2e1adfc46e646ca046
Kasra-ebr <kasraebr1378@gmail.com>
2025-07-09 20:08:17 -0400

    Commit

--- src/data/projectList.jsx
+++ src/data/projectList.jsx
@@ -1,29 +1,32 @@
- export const projects = [
-    {
-      id: 1,
-      video: 'src/video/TravelBook1.mp4', 
-      category: 'Web Design',
-      title: 'Booking-Hotel-App',
-      description: 'A modern travel assistant web application built with React, TypeScript, React.',
-      price: 49,
-      link: '#',
-    },
-    {
-      id: 2,
-      video: 'video/FREELANCE.mp4  autoPlay muted controls', 
-      category: 'Freelancer-App',
-      title: 'Freelancer-App',
-      description: 'A complete freelance management platform to handle projects, proposals, and collaboration. Built to simplify workflows for freelancers and clients alike.',
-      price: 79,
-      link: '#',
-    },
-    {
-      id: 3,
-      video: 'src/video/RICKANDMORTY1.mp4', 
-      category: 'UI/UX',
-      title: 'RickandMorty-App',
-      description: 'A React app using functional components, React Hooks, and Context API for global state management.',
-      price: 59,
-      link: '#',
-    },
-  ];
+export const projects = [
+  {
+    id: 1,
+    video: '/video/TravelBook1.mp4',
+    category: 'Web Design',
+    title: 'Booking-Hotel-App',
+    description:
+      'A modern travel assistant web application built with React, TypeScript, React.',
+    price: 49,
+    link: '#',
+  },
+  {
+    id: 2,
+    video: '/video/FREELANCE.mp4',
+    category: 'Freelancer-App',
+    title: 'Freelancer-App',
+    description:
+      'A complete freelance management platform to handle projects, proposals, and collaboration. Built to simplify workflows for freelancers and clients alike.',
+    price: 79,
+    link: '#',
+  },
+  {
+    id: 3,
+    video: '/video/RICKANDMORTY1.mp4',
+    category: 'UI/UX',
+    title: 'RickandMorty-App',
+    description:
+      'A React app using functional components, React Hooks, and Context API for global state management.',
+    price: 59,
+    link: '#',
+  },
+];
